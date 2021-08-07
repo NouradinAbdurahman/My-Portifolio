d740519c07d1f5a01ff35be8706613ce4309031c
Nouradin Abdurahman <70664023+NouradinAbdurahman@users.noreply.github.com>
2021-08-07 07:28:51 +0300

    added some features

--- JS/app.js
+++ JS/app.js
@@ -1,11 +1,21 @@
+// Mobile Navbar
 const mobilebtn = document.getElementById("mobile-cta")
 const nav = document.querySelector('.navbar')
 const mobilebtnExit = document.getElementById('mobile-exit');
-
-// Function
 mobilebtn.addEventListener('click',() =>{
     nav.classList.add('menu-btn');
 })
 mobilebtnExit.addEventListener('click',() =>{
     nav.classList.remove('menu-btn');
+})
+
+
+// Scroll To Tup
+const btnScrollToTup = document.querySelector(".scrollUp");
+btnScrollToTup.addEventListener("click",() => {
+    window.scrollTo ({
+        top:0,
+        left:0,
+        behavior: "smooth"
+    })    
 })
