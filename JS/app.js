2578690c54685f7a4a39a5a3e8cc9f7cb767ab85
NOURADDIN ⭐️ <70664023+NouradinAbdurahman@users.noreply.github.com>
2022-10-01 15:35:51 +0300

    Fixed some bugs

--- JS/app.js
+++ JS/app.js
@@ -1,20 +1,24 @@
 // Mobile Navbar
-const mobilebtn = document.getElementById("mobile-cta")
-const nav = document.querySelector('.navbar')
-const mobilebtnExit = document.getElementById('mobile-exit');
-mobilebtn.addEventListener('click',() =>{
-    nav.classList.toggle('menu-btn');
-})
-mobilebtnExit.addEventListener('click',() =>{
-    nav.classList.remove('menu-btn');
-})
+const openMenu = document.getElementById("open-menu");
+const closeMenu = document.getElementById("close-menu");
+const nav = document.querySelector('.navbar');
 
+openMenu.addEventListener('click',() =>{
+    nav.classList.add('menu-btn');
+    openMenu.style.display = "none";
+    closeMenu.style.display = "block";
+});
+closeMenu.addEventListener("click", () => {
+  nav.classList.remove("menu-btn");
+  openMenu.style.display = "block";
+  closeMenu.style.display = "none";
+});
 
 // Scroll To Top
 const btnScrollToTup = document.querySelector(".scrollUp");
 
 window.onscroll = () => {
-    if(window.scrollY >= 600) {
+    if(window.scrollY >= 500) {
         btnScrollToTup.style.display = "block";
     } else  {
         btnScrollToTup.style.display = "none";
